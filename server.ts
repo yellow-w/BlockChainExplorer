5fcd42a501b899549957351f6f2662b0c74359f4
jihyun kim <jihyunivy@gmail.com>
2022-07-05 18:35:36 +0900

    [getData] WIP: get tx, block data from DB

--- server.ts
+++ server.ts
@@ -1,13 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { sequelize } from './sequelize/models/index';
+import Block from './sequelize/models/block.model';
+import Transaction from './sequelize/models/transaction.model';
+
 dotenv.config();
 
 const { PORT } = process.env;
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('hey');
+});
+
+//blockList를 20개씩 보여줌
+app.post('/blocks', async (req, res) => {
+    const blocks = await Block.findAll();
+    res.json(blocks);
+});
+
+app.post('/transaction', async (req, res) => {
+    const txs = await Transaction.findAll();
+    res.json({});
 });
 
 app.listen(PORT, async () => {
